58d02770d81881d436b3eac4bef8321af23dbf4f
alcimarbmx <30501758+alcimarbmx@users.noreply.github.com>
2017-07-31 02:21:12 -0300

    Add files via upload

---
+++ follower.ino
@@ -0,0 +1,19 @@
+const int L1 = 12, L2 = 11, L3 = 10, L4 = 9;
+int C1, C2, C3, C4;
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 9; i < 13; i++){
+    pinMode(i, INPUT);
+    }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  C1 = digitalRead(L1);
+  C2 = digitalRead(L2);
+  C3 = digitalRead(L3);
+  C4 = digitalRead(L4);
+  delay(100);
+
+  
+}
