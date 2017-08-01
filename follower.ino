1632b218a12771da54bfa5f7a5ce6f905400901c
alcimarbmx <30501758+alcimarbmx@users.noreply.github.com>
2017-08-01 20:41:50 -0300

    Add files via upload

--- follower.ino
+++ follower.ino
@@ -1,9 +1,14 @@
 const int L1 = 12, L2 = 11, L3 = 10, L4 = 9;
+const int a = 7, b = 6, c = 5, d = 4;
 int C1, C2, C3, C4;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   for (int i = 9; i < 13; i++){
     pinMode(i, INPUT);
+    }
+  for(int i = 4; i <= 7; i++){
+    pinMode(i, OUTPUT);
     }
 }
 
@@ -13,7 +18,21 @@ void loop() {
   C2 = digitalRead(L2);
   C3 = digitalRead(L3);
   C4 = digitalRead(L4);
+  Serial.println(C1);
+  Serial.println(C2);
+  Serial.println(C3);
+  Serial.println(C4);
   delay(100);
-
+  if(C1 == HIGH){
+  digitalWrite(a, C1);
+  digitalWrite(b, C2);
+  digitalWrite(c, C3);
+  digitalWrite(d, C4);
+  }else{
+  digitalWrite(a, C1);
+  digitalWrite(b, C2);
+  digitalWrite(c, C3);
+  digitalWrite(d, C4);
+    }
   
-}
+  }
